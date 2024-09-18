0f380e992c13905b324553cd3b2b9e9df692100d
Jami40 <jnjami40@gmail.com>
2024-09-19 02:54:21 +0600

    New page added with add money and cashout

--- jS/Login.js
+++ jS/Login.js
@@ -1,9 +1,21 @@
 document.getElementById('btn-id').addEventListener('click',function(event){
     event.preventDefault();
-    console.log('Login button clicked');
+    // console.log('Login button clicked');
 
     const phoneNumber=document.getElementById('phone-number').value;
 
-    console.log(phoneNumber);
+    const pinNumber=document.getElementById('Pin-number').value;
+
+    // console.log(phoneNumber);
+
+    // console.log(pinNumber);
+
+    if(phoneNumber==='5' && pinNumber==='1234'){
+        console.log('You are logged in');
+        window.location.href='/newpage.html';
+    }
+    else{
+        alert('You made wrong command');
+    }
 
 })
