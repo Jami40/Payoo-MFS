7898e6eeb7b9d658761c47cb6c64535521097ae2
Jami40 <jnjami40@gmail.com>
2024-09-19 11:47:15 +0600

    Add muney function added

--- jS/newpage.js
+++ jS/newpage.js
@@ -1,1 +1,8 @@
-console.log('Home added');
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    // console.log('add money btn added')
+    const addNumber=document.getElementById('add-number').value;
+    // console.log(addNumber);
+    const pinNumber=document.getElementById('pin-number').value;
+    // console.log(pinNumber);
+})
